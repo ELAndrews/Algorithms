d40a9d1c63fd048635f073788ab4ef33ba96d8be
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-04-16 21:55:36 +0100

    complete making_change => without cache

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python
 
 import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(1000001)
+
+amounts = [1, 5, 10, 25, 50]
+
 
 def making_change(amount, denominations):
-  pass 
 
+    if amount == 0:
+        return 1
+    elif amount < 0:
+        return 0
+    elif len(denominations) == 0:
+        return 0
+    else:
+        return making_change(amount - denominations[0], denominations) + making_change(amount, denominations[1:])
+
+
+# print(making_change(1, amounts))
+# print(making_change(5, amounts))
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
